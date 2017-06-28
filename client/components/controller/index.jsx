7ff2ad9295f6c62fac8166111a22e7600f988fe3
roccotripaldi <tripaldir@gmail.com>
2017-06-28 00:03:22 -0400

    Adding a basic layout for controller.

---
+++ client/components/controller/index.jsx
@@ -0,0 +1,16 @@
+/**
+ * External Dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+
+class Controller extends Component {
+    render() {
+        return (
+            <div className="controller">
+                <p>Lowest card deals first.<br />Draw a card to start the game!</p>
+            </div>
+        );
+    }
+}
+
+export default Controller;
--- client/main.jsx
+++ client/main.jsx
@@ -5,11 +5,13 @@ import React from 'react';
 import Hand from 'components/hand';
 import Game from 'components/game';
 import Menu from 'components/menu/';
+import Controller from 'components/controller';
 
 let Main = () => {
     return(
         <div className="cribbage">
             <Menu />
+            <Controller />
             <Hand type="opponent" />
             <Game />
             <Hand type="player" />
